650704ad7dc101ae3ba96e446831745265cf6cf3
Baptiste Bignaud <baptiste.bignaud@edu.escp.eu>
2022-03-12 18:08:33 +0100

    Improve data collection from tweeter

--- data_twitter.py
+++ data_twitter.py
@@ -1,24 +1,51 @@
 import twitter
 from utile.get_environment import get_configuration
 from pprint import pprint
+import tweepy
+from datetime import datetime
 
 config = get_configuration()
-# print(config)
-api = twitter.Api(
-    consumer_key=config["consumer_key"],
-    consumer_secret=config["consumer_secret"],
-    access_token_key=config["access_token_key"],
-    access_token_secret=config["access_token_secret"],
-)
 
 
-def get_company(company):
-    results = api.GetSearch(
-        raw_query=f"q={company}%20&result_type=recent&since=2014-07-19&count=2&lang=en"
+def twitter_api_connection(config):
+    auth = tweepy.OAuth1UserHandler(
+        consumer_key=config["consumer_key"],
+        consumer_secret=config["consumer_secret"],
+        access_token=config["access_token_key"],
+        access_token_secret=config["access_token_secret"],
     )
-    return results
+    return auth
 
 
-results = get_company("Criteo")
-print(results[0])
-print("\n", results[0]._json.keys())
+auth = twitter_api_connection(config)
+
+api = tweepy.API(auth)
+
+
+def get_company(
+    company,
+    lang=None,
+    place=None,
+    fromdate=200603210000,
+    toDate=datetime.today().strftime("%Y%m%d%H%m"),
+    maxResults=10,
+):
+    query = (
+        f"({company} OR {company.title()} OR {company.lower()} OR {company.upper()})"
+    )
+    if lang:
+        " ".join([query, f"lang:{lang}"])
+    if place:
+        " ".join([query, f"place:{place}"])
+
+    output = api.search_full_archive(
+        label="draft",
+        query=f"({company} OR {company.title()} OR {company.lower()} OR {company.upper()}) lang:{lang} place:USA",
+        fromDate=fromdate,
+        toDate=toDate,
+        maxResults=maxResults,
+    )
+    return output[0]
+
+
+print(get_company("Criteo", "en", "USA")._json.keys())
