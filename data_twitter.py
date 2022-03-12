a5ca78346dedc3600cf889ec72267a58b44cca6a
Baptiste Bignaud <baptiste.bignaud@edu.escp.eu>
2022-03-12 12:40:55 +0100

    Update data collection

--- data_twitter.py
+++ data_twitter.py
@@ -1,7 +1,9 @@
 import twitter
 from utile.get_environment import get_configuration
+from pprint import pprint
 
 config = get_configuration()
+# print(config)
 api = twitter.Api(
     consumer_key=config["consumer_key"],
     consumer_secret=config["consumer_secret"],
@@ -9,8 +11,14 @@ api = twitter.Api(
     access_token_secret=config["access_token_secret"],
 )
 
-results = api.GetSearch(
-    raw_query="q=twitter%20&result_type=recent&since=2014-07-19&count=100"
-)
 
-print(results[0], "\n", results[0]._json.keys())
+def get_company(company):
+    results = api.GetSearch(
+        raw_query=f"q={company}%20&result_type=recent&since=2014-07-19&count=2&lang=en"
+    )
+    return results
+
+
+results = get_company("Criteo")
+print(results[0])
+print("\n", results[0]._json.keys())
